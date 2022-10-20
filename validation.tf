4b628cf056f04420a1216ba7a77aba2a54b9b00b
Maciej Wilczyński <maciej@lupine.software>
2022-10-20 22:31:37 +0200

    Update dependencies

--- validation.tf
+++ validation.tf
@@ -1,6 +1,6 @@
 module "assert_same_account" {
-  source        = "Invicton-Labs/assertion/null"
-  version       = "~>0.2.1"
+  source        = "Kalepa/assertion/null"
+  version       = "~> 0.2"
   condition     = var.bucket_a_module.account_id == var.bucket_b_module.account_id
   error_message = "Replication may only be configured with this module for two buckets in the same AWS account."
 }
